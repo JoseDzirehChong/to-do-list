283b51f5413648aaf96a85b66b672983ecd2a673
Jose Dzireh Chong <josedzirehchong@gmail.com>
2017-03-10 04:35:03 +1100

    update permissionsChecker.py

--- to-do-list/permissionsChecker.py
+++ to-do-list/permissionsChecker.py
@@ -2,5 +2,5 @@ import os
 
 folderFilepath = os.path.expanduser(os.path.join("~", "Documents", "joseDzirehChongToDoList"))
 savefileFilepath = os.path.expanduser(os.path.join("~", "Documents", "joseDzirehChongToDoList", "toDoListSaveFile.json"))
-print("joseDzirehChongToDoList has %s permissions" % oct(os.stat(folderFilepath).st_mode))
-print("toDoListSaveFile.json has %s permissions" % oct(os.stat(savefileFilepath).st_mode))
+print("joseDzirehChongToDoList has %s permissions" % oct(os.stat(folderFilepath).st_mode)[-3:])
+print("toDoListSaveFile.json has %s permissions" % oct(os.stat(savefileFilepath).st_mode)[-3:])
