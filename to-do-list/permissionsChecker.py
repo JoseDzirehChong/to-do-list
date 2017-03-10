5ad0130b9aa54dc3aee498fdedad8acf82bd29f1
Jose Dzireh Chong <josedzirehchong@gmail.com>
2017-03-10 18:05:37 +1100

    permissionsChecker doesn't work very well, hopefully toDoList is fixed

--- to-do-list/permissionsChecker.py
+++ to-do-list/permissionsChecker.py
@@ -1,18 +1,21 @@
 import os
-import pwd
+import pathlib
 
 FOLDER_FILEPATH = os.path.expanduser(os.path.join("~", "Documents", "joseDzirehChongToDoList"))
 SAVEFILE_FILEPATH = os.path.expanduser(os.path.join("~", "Documents", "joseDzirehChongToDoList", "toDoListSaveFile.json"))
 
+pathlibFolderPath = pathlib.Path(FOLDER_FILEPATH)
+pathlibSavefilePath = pathlib.Path(SAVEFILE_FILEPATH)
+
 folderPermissions = oct(os.stat(FOLDER_FILEPATH).st_mode)[-3:]
 savefilePermissions = oct(os.stat(SAVEFILE_FILEPATH).st_mode)[-3:]
 
-folderOwner = pwd.getpwuid(os.stat(FOLDER_FILEPATH).st_uid).pw_name
-savefileOwner = pwd.getpwuid(os.stat(SAVEFILE_FILEPATH).st_uid).pw_name
+folderOwner = pathlibFolderPath.owner
+savefileOwner = pathlibSavefilePath.owner
 
 def printPermissionsAndOwnership():
-    print("joseDzirehChongToDoList has {} permissions and is owned by {}".format(folderPermissions, folderOwner)
+    print("joseDzirehChongToDoList has {} permissions and is owned by {}".format(folderPermissions, folderOwner))
     print()
-    print("toDoListSaveFile.json has {} permissions and is owned by {}".format(savefilePermissions, savefileOwner)
+    print("toDoListSaveFile.json has {} permissions and is owned by {}".format(savefilePermissions, savefileOwner))
 
 printPermissionsAndOwnership()
